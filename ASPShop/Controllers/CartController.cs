af17f61b8d3d6f8e4886888f2169e07c72fb1ce5
the-Dust <dust_@inbox.ru>
2017-10-14 18:03:09 +0400

    Товары добавляются в корзину и отображаются в ней.

---
+++ ASPShop/Controllers/CartController.cs
@@ -0,0 +1,58 @@
+﻿using DataAccess.Entities;
+using Services.BuisnessLogic.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductService productService = null;
+
+        public CartController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            CartIndexViewModel model = new CartIndexViewModel { Cart = GetCart(), ReturnUrl = returnUrl };
+            return View(model);
+        }
+
+        public Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+
+        public RedirectToRouteResult AddToCart(int id, string returnUrl)
+        {
+            Product product = productService.GetProduct(id);
+
+            if (product != null)
+                GetCart().AddItem(product, 1);
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
+        {
+            Product product = productService.GetProduct(productId);
+
+            if (product != null)
+                GetCart().RemoveLine(product);
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+    }
+}
---
+++ ASPShop/Models/CartIndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿using DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class CartIndexViewModel
+    {
+        public Cart Cart { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+    }
+}
