df614100e3f36944d8d47b56740c6dfc37324ed0
the-Dust <dust_@inbox.ru>
2017-11-06 18:27:51 +0400

    Настроена реальная отправка почтовых оповещений о сделанных заказах

--- DataAccess/Entities/EmailOrderProcessor.cs
+++ DataAccess/Entities/EmailOrderProcessor.cs
@@ -1,6 +1,7 @@
 ﻿using DataAccess.Entities.Base;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
@@ -11,14 +12,14 @@ namespace DataAccess.Entities
 {
     public class EmailSettings
     {
-        public string MailToAddress { get; set; } = "dust_@inbox.ru";
-        public string MailFromAddress { get; set; } = "dust_@inbox.ru";
+        public string MailToAddress { get; set; } = "myneglectedmail@gmail.com";
+        public string MailFromAddress { get; set; } = "myneglectedmail@gmail.com";
         public bool UseSsl { get; set; } = true;
-        public string Username { get; set; } = "user";
-        public string Password { get; set; } = "pass";
-        public string ServerName { get; set; } = "smtp.example.com";
+        public string Username { get; set; } = "myneglectedmail@gmail.com";
+        public string Password { get; set; } = "g00dluck";
+        public string ServerName { get; set; } = "smtp.gmail.com";
         public int ServerPort { get; set; } = 587;
-        public bool WriteAsFile { get; set; } = true;
+        public bool WriteAsFile { get; set; } = false;
         public string FileLocation { get; set; } = @"d:\";
     }
 
@@ -38,8 +39,9 @@ namespace DataAccess.Entities
                 smtpClient.EnableSsl = emailSettings.UseSsl;
                 smtpClient.Host = emailSettings.ServerName;
                 smtpClient.Port = emailSettings.ServerPort;
-                smtpClient.UseDefaultCredentials = false;
+                smtpClient.UseDefaultCredentials = true;
                 smtpClient.Credentials = new NetworkCredential(emailSettings.Username, emailSettings.Password);
+                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
 
                 if (emailSettings.WriteAsFile)
                 {
@@ -56,10 +58,11 @@ namespace DataAccess.Entities
                 foreach (var line in cart.Lines)
                 {
                     var subtotal = line.Product.Cost * line.Quantity;
-                    body.AppendFormat("{0} x {1} (итого: {2:c})", line.Quantity, line.Product.Name, subtotal);
+                    body.AppendFormat("{0} x {1} (итого: {2:#.00}руб.)", line.Quantity, line.Product.Name, subtotal).AppendLine("");
                 }
 
-                body.AppendFormat("Общая стоимость: {0:c}", cart.ComputeTotalValue())
+                body.AppendFormat("Общая стоимость: {0:#.00}руб.", cart.ComputeTotalValue())
+                    .AppendLine("")
                     .AppendLine("---")
                     .AppendLine("Доставка:")
                     .AppendLine(shippingDetails.Name)
